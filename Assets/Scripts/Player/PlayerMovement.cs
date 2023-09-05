96ca42a22fa2663a97ccbae9e20c477e1d179288
Kiryl Prykhodzka <kirilkin.97@gmail.com>
2023-09-05 23:00:04 +0200

    add some basic stuff

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //detect wasd input. set movement direction to null if there is no input or 0-360 based on pressed buttons
+    }
+
+    void FixedUpdate()
+    {
+        //if movement direction != null move in the specified direction
+    }
+}

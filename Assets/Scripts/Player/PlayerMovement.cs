f97e53374ce734e86b031ffd6561794857f9b272
Kiryl Prykhodzka <kirilkin.97@gmail.com>
2023-09-06 17:01:28 +0200

    add player movement script

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,23 +1,55 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float moveSpeed = 100f;
+    [SerializeField] private Rigidbody2D playerBody;
+
+    private int movementX = 0;
+    private int movementY = 0;
+
+    private void Start()
     {
-        
+        playerBody = gameObject.GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //detect wasd input. set movement direction to null if there is no input or 0-360 based on pressed buttons
+        if ((Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.S)) || (!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S)))
+        {
+            movementY = 0;
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                movementY = 1;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                movementY = -1;
+            }
+        }
+
+        if ((Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D)) || (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D)))
+        {
+            movementX = 0;
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.D))
+            {
+                movementX = 1;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                movementX = -1;
+            }
+        }
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        //if movement direction != null move in the specified direction
+        playerBody.AddForce(new Vector2(movementX * Time.deltaTime * moveSpeed, movementY * Time.deltaTime * moveSpeed));
     }
 }
